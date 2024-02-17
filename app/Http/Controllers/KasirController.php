876df451f51ab5a91b03dc76cb027a33473d96dd
ca-kraa <alexandrokingnabil@gmail.com>
2024-02-17 12:22:36 +0700

    #4 Show Data Penjualan

--- app/Http/Controllers/KasirController.php
+++ app/Http/Controllers/KasirController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Penjualan;
 use Illuminate\Http\Request;
 
 class KasirController extends Controller
 {
-    //
+    public function indexPenjualan()
+    {
+        $penjualan = Penjualan::all();
+        return response()->json($penjualan);
+    }
+
+    
 }
